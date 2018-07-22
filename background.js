58c81c66efd0ebbb2ab8ddbc24fda7fb0718a557
phani <pasupulaphani@gmail.com>
2018-07-22 12:56:02 +0100

    added bg jobs

--- background.js
+++ background.js
@@ -22,3 +22,81 @@ chrome.runtime.onInstalled.addListener(function() {
 
   });
 });
+
+var bkg = chrome.extension.getBackgroundPage();
+
+chrome.runtime.onInstalled.addListener(function(details) {
+  // register callback for alarm
+  chrome.alarms.onAlarm.addListener(onAlarm);
+  
+  // synchronize every 1 minute
+  chrome.alarms.create("extension.alarm1", {periodInMinutes: 1});
+  
+  console.log("extension is running");
+ });
+
+
+function onAlarm(alarm) {
+  start();
+ };
+
+ 
+function start() {
+  chrome.alarms.create("Start", {periodInMinutes:1});
+
+  $.ajax({
+    url: "http://localhost:3000/data",
+    dataType : 'json',
+    success: function(data) {
+      bkg.console.log('data', data);
+
+      const zrx1dsell = data
+        .filter(a => {
+          return hoursFromNow(a.timeStamp) < 24
+        })
+        .reduce(function (accumulator, currentValue) {
+          return accumulator + currentValue.value;
+        }, 0);
+      console.log(zrx1dsell)
+      chrome.storage.sync.set({
+        zrx1dsell: zrx1dsell
+      }, function() {
+        console.log("set", zrx1dsell)
+      });
+
+
+      const zrx1wsell = data
+        .filter(a => {
+          return hoursFromNow(a.timeStamp) < (24 * 7)
+        })
+        .reduce(function (accumulator, currentValue) {
+          return accumulator + currentValue.value;
+        }, 0);
+      console.log(zrx1wsell)
+      chrome.storage.sync.set({
+        zrx1wsell: zrx1wsell
+      }, function() {
+        console.log("set", zrx1wsell)
+      });
+    },
+    error: function(e) {
+      bkg.console.log('e', e);
+    }
+  });
+
+  chrome.notifications.create({
+    type: "basic",
+    title: "onAlarm",
+    iconUrl: "images/finexsider32.png",
+    message: 'some msg'
+  });
+}
+start();
+
+
+function hoursFromNow(timestamp) {
+  var currentTime = moment();
+  var timeStore = moment.unix(timestamp);
+  // console.log(timestamp, moment.unix(timestamp), currentTime.diff(timeStore, 'h'))
+  return currentTime.diff(timeStore, 'h');
+}
