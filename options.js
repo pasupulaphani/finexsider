2fe505af9e9a9ad62530bf10684cd00dfd1c8074
slm15 <samuel.mnet@gmail.com>
2018-07-22 10:48:33 +0100

    prune code

--- options.js
+++ options.js
@@ -1,10 +1,8 @@
 // Saves options to chrome.storage
 function save_options() {
   var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    favoriteColor: color
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -18,13 +16,11 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
+  // Use default value color = 'red' 
   chrome.storage.sync.get({
     favoriteColor: 'red',
-    likesColor: true
   }, function(items) {
     document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
