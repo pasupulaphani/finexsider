720c0544a845a812b97ad945aa478dd46406a9d4
slm15 <samuel.mnet@gmail.com>
2018-07-28 14:38:24 +0200

    UI improvement (viz. bootstrap)

--- popup.js
+++ popup.js
@@ -1,28 +1,24 @@
 'use strict';
 
-// let changeColor = document.getElementById('changeColor');
-
 let illustration = document.getElementById('illustration');
 let illustration2 = document.getElementById('illustration2');
 
 chrome.storage.sync.get('zrx1dsell', function(data) {
-  // changeColor.setAttribute('value', data.color);
   illustration.innerHTML = Math.round(data.zrx1dsell);
 });
 chrome.storage.sync.get('zrx1wsell', function(data) {
-  // changeColor.setAttribute('value', data.color);
   illustration2.innerHTML = Math.round(data.zrx1wsell);
 });
 
 
-changeColor.onclick = function(element) {
-  let illustration = document.getElementById('illustration');
+plusbutton.onclick = function(element) {
+  // let illustration = document.getElementById('illustration');
+  console.log(hi)
 
-
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  // let color = element.target.value;
+  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     // chrome.tabs.executeScript(
     //     tabs[0].id,
     //     {code: 'document.body.style.backgroundColor = "' + color + '"; console.log("hi")' });
-  });
+  // });
 };
